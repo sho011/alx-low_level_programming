9d6961c20473213451aadfee3201438d7383b8e3
sho011 <hamzabadra011@gmail.com>
2023-03-21 11:31:29 -0700

    loop

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * * _abs - checks for absolute value 
+ * _abs - checks for absolute value
  *
  * @a: parameter to be checked
  *
  * Return: always a
  */
+
 
 int _abs(int a)
 {
