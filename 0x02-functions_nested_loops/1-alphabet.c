22d9725830fbfd9e295b16693563ca35e9e65dd9
sho011 <hamzabadra011@gmail.com>
2023-03-21 08:47:10 -0700

    bettyuza

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,3 +1,16 @@
 #include "main.h"
+/**
+ * print_alphabet - function that prints alphabet in lower casr
+ *
+ * Return: always 0
+ */
 
 
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		_putchar(i);
+	_putchar('\n');
+}
